a95e51cf4d687d56751cd8c61b1572bbcdc5d946
Liberbell <liberbell@gmail.com>
2023-11-07 08:41:58 +0900

    main4

--- sample1/main.py
+++ sample1/main.py
@@ -5,3 +5,10 @@ import pandas as pd
 st.title("Streamlit introduction")
 
 st.write("DataFrame")
+
+df = pd.DataFrame({
+    "First row": [1, 2, 3, 4],
+    "Second row": [10, 20, 30, 40]
+})
+
+st.write(df)
