b843824f4615bcbf58ed9d4eea14c6e3e82b1212
Liberbell <liberbell@gmail.com>
2023-11-08 08:23:23 +0900

    main1

--- sample1/main.py
+++ sample1/main.py
@@ -16,6 +16,10 @@ df1 = pd.DataFrame(
     columns=["a", "b", "c"]
 )
 
+df2 = pd.DataFrame(
+    np.random.rand(100, 3)/(50, 50) + [35.69, 139.70],
+    columns=["lat", "lon"]
+)
 # st.write(df)
 # st.table(df.style.highlight_max(axis=0))
 
@@ -32,6 +36,6 @@ import pandas as pd
 
 """
 
-st.line_chart(df1)
-st.area_chart(df1)
-st.bar_chart(df1)
+# st.line_chart(df1)
+# st.area_chart(df1)
+# st.bar_chart(df1)
