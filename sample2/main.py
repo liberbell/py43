4d12270a58ee80f32de059c115a0a46dc734963c
Liberbell <liberbell@gmail.com>
2023-11-10 15:28:44 +0900

    main12

--- sample2/main.py
+++ sample2/main.py
@@ -8,26 +8,28 @@ days = 20
 tickers = {
     "Apple": "AAPL",
     "Microsoft": "MSFT",
-    "Facebook": "FB",
+    "Facebook": "META",
 }
-company = "Apple"
+# company = "Facebook"
+df = pd.DataFrame()
+for comp in tickers.keys():
+    tkr = yf.Ticker(tickers[company])
+    hist = tkr.history(period=f"{days}d")
+    # print(hist_aapl)
 
-tkr = yf.Ticker(tickers[company])
-hist = tkr.history(period=f"{days}d")
-# print(hist_aapl)
+    # msft = yf.Ticker("MSFT")
+    # hist_msft = msft.history(period=f"{days}d")
+    # print(hist_msft)
 
-# msft = yf.Ticker("MSFT")
-# hist_msft = msft.history(period=f"{days}d")
-# print(hist_msft)
+    # print([hist_aapl, hist_msft], axix=1)
 
-# print([hist_aapl, hist_msft], axix=1)
+    hist.index = hist.index.strftime("%d %B %Y")
+    # print(hist_aapl.head())
 
-hist.index = hist.index.strftime("%d %B %Y")
-# print(hist_aapl.head())
+    hist = hist[["Close"]]
+    hist.columns = [company]
+    hist = hist.T
+    hist.index.name = "Name"
+    pd.concat([df, hist])
 
-hist = hist[["Close"]]
-hist.columns = [company]
-hist = hist.T
-hist.index.name = "Name"
-
-print(hist)
+    # print(hist)
