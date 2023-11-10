f6482e8835620513d64ac00fafce3c5607008da3
Liberbell <liberbell@gmail.com>
2023-11-10 14:49:20 +0900

    main3

--- sample2/main.py
+++ sample2/main.py
@@ -6,8 +6,8 @@ import yfinance as yf
 
 aapl = yf.Ticker("AAPL")
 days = 20
-hist = aapl.history(period=f"{days}d")
-print(hist)
+hist_aapl = aapl.history(period=f"{days}d")
+print(hist_aapl)
 
 msft = yf.Ticker("MSFT")
 hist_msft = msft.history(period=f"{days}d")
