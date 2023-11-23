45d71b0b375b7075682458c3b7dc2b6ea89e41f2
Liberbell <liberbell@gmail.com>
2023-11-23 10:51:31 +0900

    main10

--- sample2/main.py
+++ sample2/main.py
@@ -34,3 +34,10 @@ def get_data(days, tickers):
     for company in tickers.keys():
         tkr = yf.Ticker(tickers[company])
         hist = tkr.history(period=f"{days}d")
+        hist.index = hist.index.strftime("%d %D %Y")
+        hist = hist[["Close"]]
+        hist.columns = [company]
+        hist = hist.T
+        hist.index.name = "Name"
+        df = pd.concat([df, hist])
+    return df
