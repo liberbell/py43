5411c425a050fb59901467cdf01f48ab6b479bca
Liberbell <liberbell@gmail.com>
2023-11-23 10:42:29 +0900

    main8

--- sample2/main.py
+++ sample2/main.py
@@ -4,17 +4,29 @@ import streamlit as st
 import yfinance as yf
 
 
-st.title("# American Stock Price")
+tickers = {
+    "Apple": "AAPL",
+    "Facebook": "META",
+    "Google": "GOOGL",
+    "Microsoft": "MSFT",
+    "Netflix": "NTLX",
+    "Amazon": "AMZN"
+}
+
+st.title("American Stock Price")
 
 st.sidebar.write("""
-                 GAFA Stock price
-
-                 Visualize tool
+                 # GAFA Stock price
+                 # Visualize tool
                  """)
 
 st.sidebar.write("Display days")
-st.sidebar.slider("Days", 1, 50, 20)
 
-st.write("""
-         ### GAFA Stock Price past 30 days
+
+days = st.sidebar.slider("Days", 1, 50, 20)
+
+st.write(f"""
+         ### GAFA Stock Price past **{days} days**
          """)
+
+def get_data(days, tickers)
