16cf7ca7a244ff5408689f260e5b37f2132d23b2
Liberbell <liberbell@gmail.com>
2023-11-23 10:17:08 +0900

    main5

--- sample2/main.py
+++ sample2/main.py
@@ -2,9 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 import altair as alt
+import streamlit as st
 
 # %matplotlib inline
 
+st.title = "American Stock price"
 days = 20
 tickers = {
     "Apple": "AAPL",
@@ -15,53 +17,53 @@ tickers = {
     "Amazon": "AMZN"
 }
 
-def get_data(days, tickers):
-    df = pd.DataFrame()
-    for company in tickers.keys():
-        tkr = yf.Ticker(tickers[company])
-        print(tkr.info)
-        hist = tkr.history(period=f"{days}d")
-        # print(hist_aapl)
+# def get_data(days, tickers):
+#     df = pd.DataFrame()
+#     for company in tickers.keys():
+#         tkr = yf.Ticker(tickers[company])
+#         print(tkr.info)
+#         hist = tkr.history(period=f"{days}d")
+#         # print(hist_aapl)
 
-        # msft = yf.Ticker("MSFT")
-        # hist_msft = msft.history(period=f"{days}d")
-        # print(hist_msft)
+#         # msft = yf.Ticker("MSFT")
+#         # hist_msft = msft.history(period=f"{days}d")
+#         # print(hist_msft)
 
-        # print([hist_aapl, hist_msft], axix=1)
+#         # print([hist_aapl, hist_msft], axix=1)
 
-        hist.index = hist.index.strftime("%d %B %Y")
-        # print(hist_aapl.head())
+#         hist.index = hist.index.strftime("%d %B %Y")
+#         # print(hist_aapl.head())
 
-        hist = hist[["Close"]]
-        hist.columns = [company]
-        hist = hist.T
-        hist.index.name = "Name"
-        df = pd.concat([df, hist])
-    return df
+#         hist = hist[["Close"]]
+#         hist.columns = [company]
+#         hist = hist.T
+#         hist.index.name = "Name"
+#         df = pd.concat([df, hist])
+#     return df
 
     # print(hist)
-print(get_data(days, tickers))
-companies = ["Apple", "Facebook"]
-data = df.loc(companies)
-print(data)
+# print(get_data(days, tickers))
+# companies = ["Apple", "Facebook"]
+# data = df.loc(companies)
+# print(data)
 
-data.sort_index()
-data = data.T.reset_index()
-data = pd.melt(data, id_vars=["Date"]).rename(
-    columns={'value': 'Stock Price(USD)'}
-)
+# data.sort_index()
+# data = data.T.reset_index()
+# data = pd.melt(data, id_vars=["Date"]).rename(
+#     columns={'value': 'Stock Price(USD)'}
+# )
 
-ymin, ymax = 200, 300
+# ymin, ymax = 200, 300
 
-chart = (
-    alt.Chart(data)
-    .mark_line(opacity=0.8, clip=True)
-    .encode(
-        x="Date:T",
-        y=alt.Y("Stock Prices(USD):Q",
-        stack=None,
-        scale=alt.Scale(domain=[ymin, ymax])),
-        color="Name:N"
-    )
-)
-chart
+# chart = (
+#     alt.Chart(data)
+#     .mark_line(opacity=0.8, clip=True)
+#     .encode(
+#         x="Date:T",
+#         y=alt.Y("Stock Prices(USD):Q",
+#         stack=None,
+#         scale=alt.Scale(domain=[ymin, ymax])),
+#         color="Name:N"
+#     )
+# )
+# chart
