302293733f17edac3fd8a52afd2de640dcf9eef1
Liberbell <liberbell@gmail.com>
2023-11-23 10:31:15 +0900

    main6

--- sample2/main.py
+++ sample2/main.py
@@ -1,69 +1,15 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import yfinance as yf
 import altair as alt
 import streamlit as st
+import yfinance as yf
 
-# %matplotlib inline
 
-st.title = "American Stock price"
-days = 20
-tickers = {
-    "Apple": "AAPL",
-    "Microsoft": "MSFT",
-    "Facebook": "META",
-    "Google": "GOOGL",
-    "Netflix": "NFLX",
-    "Amazon": "AMZN"
-}
+st.title("American Stock Price")
 
-# def get_data(days, tickers):
-#     df = pd.DataFrame()
-#     for company in tickers.keys():
-#         tkr = yf.Ticker(tickers[company])
-#         print(tkr.info)
-#         hist = tkr.history(period=f"{days}d")
-#         # print(hist_aapl)
+st.sidebar.write("""
+                 GAFA Stock price
 
-#         # msft = yf.Ticker("MSFT")
-#         # hist_msft = msft.history(period=f"{days}d")
-#         # print(hist_msft)
+                 Visualize tool
+                 """)
 
-#         # print([hist_aapl, hist_msft], axix=1)
-
-#         hist.index = hist.index.strftime("%d %B %Y")
-#         # print(hist_aapl.head())
-
-#         hist = hist[["Close"]]
-#         hist.columns = [company]
-#         hist = hist.T
-#         hist.index.name = "Name"
-#         df = pd.concat([df, hist])
-#     return df
-
-    # print(hist)
-# print(get_data(days, tickers))
-# companies = ["Apple", "Facebook"]
-# data = df.loc(companies)
-# print(data)
-
-# data.sort_index()
-# data = data.T.reset_index()
-# data = pd.melt(data, id_vars=["Date"]).rename(
-#     columns={'value': 'Stock Price(USD)'}
-# )
-
-# ymin, ymax = 200, 300
-
-# chart = (
-#     alt.Chart(data)
-#     .mark_line(opacity=0.8, clip=True)
-#     .encode(
-#         x="Date:T",
-#         y=alt.Y("Stock Prices(USD):Q",
-#         stack=None,
-#         scale=alt.Scale(domain=[ymin, ymax])),
-#         color="Name:N"
-#     )
-# )
-# chart
+st.sidebar.write("Display days")
