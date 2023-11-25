445c6adef40c4e75ff4a9e1b7b3c8cdbbe0a7efb
Liberbell <liberbell@gmail.com>
2023-11-25 15:15:13 +0900

    stockapp5

--- sample2/stockapp.py
+++ sample2/stockapp.py
@@ -10,3 +10,30 @@ st.sidebar.write("""
                  """)
 st.sidebar.write("Select Display days")
 days = st.sidebar.slider("Days", 1, 50, 20)
+
+st.write(f"""
+         ### GAFA Stock Price past **{days} days**
+         """)
+
+days = 20
+tickers = {
+    "Apple": "AAPL",
+    "Facebook": "META",
+    "Google": "GOOGL",
+    "Microsoft": "MSFT",
+    "Netflix": "NFLX",
+    "Amazon": "AMZN"
+}
+
+def get_data(dats, tickers):
+  df = pd.DataFrame()
+  for company in tickers.keys():
+    tkr = yf.Ticker(tickers[company])
+    hist = tkr.history(period=f"{days}d")
+    hist.index = hist.index.strftime("%d %B %Y")
+    hist = hist[["Close"]]
+    hist.columns = [company]
+    hist = hist.T
+    hist.index.name = "Name"
+    df = pd.concat([df, hist])
+  return df
